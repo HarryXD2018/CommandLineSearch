e67052f12c9caa996f66f362e8d6c64306ebbd52
H.Chen <chenhj2000@163.com>
2020-10-05 19:19:40 +0800

    Solve argparse space problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 import csv
-import os, sys
+import os
 
 
 if __name__ == '__main__':
@@ -9,18 +9,22 @@ if __name__ == '__main__':
     parser.add_argument('-e', type=str)
     args = parser.parse_args()
     print(args)
-    path = r'C:\...\cmd_bing\dist\\'        # Add your PATH dir here
+    path = r'C:\...\dist\\'        # Add your PATH dir here
     csv_file = csv.reader(open(path+'default_database.csv', 'r', encoding='UTF8'))
     data_item = [row for row in csv_file]
+    keyword = args.keyword
+    keyword: str
+    keyword = keyword.replace(' ', '+')
+    print(keyword)
     for data in data_item:
         engine = data[0]
         url_link = data[1]
         print(engine, url_link)
         if engine == args.e:
-            url = url_link.format(args.keyword)
+            url = url_link.format(keyword)
             os.system(r'start "C:\Program Files\Google\Chrome\Application\chrome.exe" {}'.format(url))
             break
     else:
-        url = data_item[1][1].format(args.keyword)
+        url = data_item[1][1].format(keyword)
     print(url)
     os.system(r'start "C:\Program Files\Google\Chrome\Application\chrome.exe" {}'.format(url))
